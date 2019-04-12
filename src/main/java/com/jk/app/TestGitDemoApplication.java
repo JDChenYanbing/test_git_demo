85251469396bc2fcaacf773c9644c395e969459f
JDChenYanbing <625265643@qq.com>
2019-04-12 18:47:56 +0800

    注é初始化项目

---
+++ src/main/java/com/jk/app/TestGitDemoApplication.java
@@ -0,0 +1,12 @@
+package com.jk.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGitDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestGitDemoApplication.class, args);
+    }
+}
